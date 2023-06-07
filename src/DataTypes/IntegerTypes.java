4e72916942577ab08067290fdd67173f9e6f8d12
K Pavan Kumar Reddy <001iampavankumar@gmail.com>
2023-06-07 10:28:02 +0530

    Initial Commit

---
+++ src/DataTypes/IntegerTypes.java
@@ -0,0 +1,16 @@
+package DataTypes;
+
+public class IntegerTypes {
+    public static void main(String[] args) {
+
+        byte a = 12;//-128 to 127
+
+        short b = 1245; // -32768 to +32767
+
+        int c = 67547; // -2147483648 to -2147483647
+
+        long d = 8675643543L; // -9223372036854775808 to 9223372036854775807
+
+        System.out.println(a+" : "+b+" : "+c+" : "+d);
+    }
+}
