ccd1388029fd9b7eb76b5b97fe809099448d5eba
K Pavan Kumar Reddy <001iampavankumar@gmail.com>
2023-08-03 21:36:32 +0530

    Initial Commit

---
+++ src/Strings/StringBuilder/BasicFunctions.java
@@ -0,0 +1,12 @@
+package Strings.StringBuilder;
+import java.util.*;
+public class BasicFunctions {
+    public static void main(String[] args) {
+        StringBuilder s = new StringBuilder("HelloGoodMorning");
+
+        s.insert(5,' ');
+        s.insert(10,' ');
+        System.out.println(s);
+
+    }
+}
