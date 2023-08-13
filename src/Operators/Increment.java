e60c1fc84016db170a876e2630178a4242c04211
K Pavan Kumar Reddy <001iampavankumar@gmail.com>
2023-08-13 22:51:41 +0530

    commit

---
+++ src/Operators/Increment.java
@@ -0,0 +1,10 @@
+package Operators;
+
+public class Increment {
+    public static void main(String[] args) {
+
+        int a += 5;
+        a += a++ + ++a;
+        System.out.println(a);
+    }
+}
